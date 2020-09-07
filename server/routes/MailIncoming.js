be7d11d011555b7923b6c146ba493e7e8e2bc890
Sasi <>
2020-09-06 23:24:26 -0400

    update

--- server/routes/MailIncoming.js
+++ server/routes/MailIncoming.js
@@ -1,6 +1,6 @@
 var Config=require('../config.js');
 
-exports.receiveIncomingMail = function(db, userId, params, callback)
+exports.API_ReceiveIncomingMail = function(db, userId, params, callback)
 {
 	console.log(userId, params);
 	return callback(null, "OK");
